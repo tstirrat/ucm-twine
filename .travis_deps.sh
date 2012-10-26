e1036c89c743dfaf796e939b9702d40a36acb356
tstirrat <tim.stirrat@gmail.com>
2012-10-26 21:39:09 +1100

    hide more unneccessary output in travis script

--- .travis_deps.sh
+++ .travis_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 FILE=deps.zip
-wget http://stirrat.org/$FILE
+wget -q http://stirrat.org/$FILE
 
 openssl aes-256-cbc -d -in $FILE -out deps.tar.gz -pass env:DEC
 
@@ -9,6 +9,6 @@ mkdir ucm-twine-deps
 tar -zxf deps.tar.gz -C ucm-twine-deps
 ucm-twine-deps/install.sh
 
-rm $FILE
-rm deps.tar.gz
+rm -f $FILE
+rm -f deps.tar.gz
 rm -rf ucm-twine-deps/
